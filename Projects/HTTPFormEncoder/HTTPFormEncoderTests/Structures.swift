9d0eaa68aed858a152694b7d4f54fde8918f7af3
Richard Stelling <rich@lionheartapplications.com>
2019-02-08 14:16:44 +0000

    wip

--- Projects/HTTPFormEncoder/HTTPFormEncoderTests/Structures.swift
+++ Projects/HTTPFormEncoder/HTTPFormEncoderTests/Structures.swift
@@ -43,5 +43,85 @@ struct JSON: Codable {
     let deep: Deep
 }
 
+struct NestedSingleValues: Codable {
+    enum Name: String, Codable {
+        case funk = "funk"
+        case soul = "soul"
+    }
+    
+    enum Pie: Int, Codable {
+        case 🍎 = 0
+        case 🎃 = 1599
+    }
+    
+    let name: Name
+    let food: Pie
+}
+
+struct SingleValues: Codable {
+    
+    enum Label: String, Codable {
+        case abc1 = "💩"
+        case efg = "🤖"
+    }
+    
+    enum Values: Int, Codable {
+        case egg = 128
+        case toast = 256
+    }
+    
+    let label: Label
+    //let more: [Label] //this fails
+    
+    let info: NestedSingleValues
+    
+    let val: Values
+}
+
 // ---------------------------------------------------------------------------------------------------------------------------------------------------//
+
+struct Layer1: Codable {
+    let l1a: String?
+    let l1b: Int
+}
+
+struct Layer2: Codable {
+    let l2a: Double?
+    let l2b: Layer1
+}
+
+struct Layer3: Codable {
+    let l3a: Layer2?
+    let l3b: Layer2
+}
+
+struct Layer4: Codable {
+    let l4a: Bool?
+    let l4b: Layer3
+}
+
+struct JSON2: Codable {
+    
+    let thing: [String]
+    
+    let snufkin: Layer1
+    
+    let moomin: Layer4
+}
+
+struct JSON3: Codable {
+    
+    let stuff: [Layer1]
+    
+}
+
+struct Nonsense: Codable {
+    
+    let pup: [[String]]
+    let birb:[[[Int]]]
+}
+struct JSON4: Codable {
+    let bleh : Nonsense
+}
+
 
