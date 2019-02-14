4ab611878aed30c4e90ab4aaa4657034844f6c33
Richard Stelling <rich@lionheartapplications.com>
2019-02-14 19:40:58 +0000

    Added macOS support to HTTPFormEncoder

---
+++ Projects/HTTPFormEncoder/HTTPFormEncoder-macOS/HTTPFormEncoder_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HTTPFormEncoder_macOS.h
+//  HTTPFormEncoder-macOS
+//
+//  Created by Richard Stelling on 14/02/2019.
+//  Copyright © 2019 Lionheart Applications Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTTPFormEncoder_macOS.
+FOUNDATION_EXPORT double HTTPFormEncoder_macOSVersionNumber;
+
+//! Project version string for HTTPFormEncoder_macOS.
+FOUNDATION_EXPORT const unsigned char HTTPFormEncoder_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPFormEncoder_macOS/PublicHeader.h>
+
+
