410f2546055beb39ed443f3419fd2686519bebb9
Richard Stelling <rich@lionheartapplications.com>
2019-02-14 20:02:25 +0000

    Flatten project to fix SPM build issues

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "./Projects/HTTPFormEncoder"),
+        //.package(path: "./Projects/HTTPFormEncoder"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HTTPFormRequest",
-            dependencies: ["HTTPFormEncoder"],
+            dependencies: [],
             path: "./Projects/HTTPFormRequest/HTTPFormRequest/"),
 //        .testTarget(
 //            name: "HTTPFormRequestTests",
